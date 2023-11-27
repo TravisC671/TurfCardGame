1198ccf58aa98a3962dfb8a2d876ca6ec57aa2fb
TravisC671 <travisc088@gmail.com>
2023-11-27 11:04:53 -0700

    added cards from server

--- app/game/[gameID]/AvailableCards.tsx
+++ app/game/[gameID]/AvailableCards.tsx
@@ -1,22 +1,28 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from './page.module.css'
 import Card from './Card'
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux/es/exports";
 import { selectCardState, setSelectedCard } from "./store/cardSlice";
+import { socket } from "./socket";
 
 export default function AvailableCards(props) {
-
+  const [cardHand, setCardHand] = useState([])
 	const card = useSelector(selectCardState);
 
-  let cardHand = [0, 1, 2, 3]
+  useEffect(() => {
+    socket.on('dealHand', (data) => {
+      setCardHand(data.cards)
+    })
+    return
+  }, [])
   
   const dispatch = useDispatch();
 
   const selectCard = (id) => {
-    dispatch(setSelectedCard(id))
+    dispatch(setSelectedCard(cardHand[id]))
     //console.log('selected card', id)
   }
 
